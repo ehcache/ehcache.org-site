b110e8b9993224f6861dd348c1d9d8eef17b27ef
House, James <James.House@terracottatech.com>
2015-08-26 16:35:13 -0400

    add lovely generated docs

---
+++ generated/2.10.0/html/ehc-all/Ehcache_Documentation_Set/pairs/pair290.js
@@ -0,0 +1,26 @@
+var pairs =
+{
+"using":{"ehcache":1}
+,"ehcache":{"jruby":1,"library":1,"including":1}
+,"jruby":{"ruby-ehcache":1,"ehcache":1,"strength":1}
+,"ruby-ehcache":{"jruby":1}
+,"library":{"makes":1}
+,"makes":{"commonly":1}
+,"commonly":{"used":1}
+,"used":{"subset":1,"directly":1}
+,"subset":{"ehcache\u0027s":1}
+,"ehcache\u0027s":{"api":1}
+,"api":{"available":1,"rails":1}
+,"available":{"jruby":1}
+,"strength":{"ehcache":1}
+,"including":{"bigmemory":1}
+,"bigmemory":{"ability":1}
+,"ability":{"cluster":1}
+,"cluster":{"terracotta":1}
+,"terracotta":{"used":1}
+,"directly":{"via":1}
+,"via":{"api":1}
+,"rails":{"caching":1}
+,"caching":{"provider":1}
+}
+;Search.control.loadWordPairs(pairs);
---
+++ generated/2.9.0/html/ehc-all/Ehcache_Documentation_Set/pairs/pair157.js
@@ -0,0 +1,7 @@
+var pairs =
+{
+"monitoring":{"management":1}
+,"management":{"using":1}
+,"using":{"jmx":1}
+}
+;Search.control.loadWordPairs(pairs);
